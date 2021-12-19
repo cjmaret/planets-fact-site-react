9fb4a385b0b1b6d6d43e03d783f91ecc57e5993b
cjmaret <cjmaret@gmail.com>
2021-12-19 14:21:59 +0700

    fix: change favicon, remove Switch import, package spacing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Header from './header/Header';
 import PlanetsData from '../databases/planets-array';
